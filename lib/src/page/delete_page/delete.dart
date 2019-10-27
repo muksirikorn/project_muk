e61155e4225cc60849e44a12a3246a2a2ef66abf
muksirikorn <mukmind369@gmail.com>
2019-10-27 17:39:35 +0700

    sdfgh

--- lib/src/page/delete_page/delete.dart
+++ lib/src/page/delete_page/delete.dart
@@ -1,1 +1,32 @@
 
+ import 'package:flutter/material.dart';
+ 
+class DeletePage extends StatefulWidget {
+  @override
+  _DeletePageState createState() => _DeletePageState();
+}
+
+class _DeletePageState extends State<DeletePage> {
+  @override
+  Widget build(BuildContext context) {
+      Container(
+                          height: 50,
+                          width: double.infinity,
+                          child: RaisedButton(
+                            color: Color(0xFFD56343),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(5),
+                            ),
+                            child: Text(
+                              "ลบข้อมูล",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {
+                            },
+                          ),
+                        );
+  }
+  }
